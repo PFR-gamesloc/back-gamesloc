841e00e574ccacd4492465dee3619f3c6c98f74d
Demeester Bryan <demeesterbryan0@gmail.com>
2023-07-27 10:49:30 +0200

    first commit

---
+++ src/test/java/pfr/backgamesloc/BackGameslocApplicationTests.java
@@ -0,0 +1,13 @@
+package pfr.backgamesloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackGameslocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
