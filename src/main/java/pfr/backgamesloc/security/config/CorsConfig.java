15ebcae04a112d5926d82df0deb5c4f728e0a68f
Demeester Bryan <demeesterbryan0@gmail.com>
2023-08-20 17:34:28 +0200

    fix

--- src/main/java/pfr/backgamesloc/security/config/CorsConfig.java
+++ src/main/java/pfr/backgamesloc/security/config/CorsConfig.java
@@ -8,7 +8,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://gamesloc.store")
+                .allowedOrigins("http://localhost:4200","http://gamesloc.store")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
